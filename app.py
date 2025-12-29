36e5fd75a70eeb0e2864cd809a259f007b183d60
Paras Vaidya <paras.vaidya.official@gmail.com>
2025-12-29 19:12:23 +0530

    Deployment setup

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
-with open("\model\hmm_model.pkl","rb") as f:
+with open("model\hmm_model.pkl","rb") as f:
     hmm_model = pickle.load(f)
 
 with open("model\regime_map.pkl","rb") as f:
